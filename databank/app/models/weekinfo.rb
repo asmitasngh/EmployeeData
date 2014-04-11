fda7c26e758bae2831a095cee5fbc97e5f46997a
asmitasngh <singhasmita80@gmail.com>
2014-04-11 16:18:56 +0530

    xlsx file

--- databank/app/models/weekinfo.rb
+++ databank/app/models/weekinfo.rb
@@ -22,6 +22,7 @@ class Weekinfo < ActiveRecord::Base
 	def self.open_spreadsheet(file)
 		case File.extname(file.original_filename)
 			when ".xls" then Roo::Excel.new(file.path, nil, :Ignore)
+			when ".xlsx" then Roo::Excelx.new(file.path, nil, :Ignore)
 			else raise "Unknown File type : #{file.original_filename}"
 		end	
 	end
